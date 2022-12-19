a669eb2ff214333866b6f5c4577aaf3f5fa970de
“Fabio” <yesares2003@gmail.com>
2022-12-19 16:20:01 +0100

    Ejercicio2

--- Ejercicios_Recuperacion/src/programas/Ejercicio2.java
+++ Ejercicios_Recuperacion/src/programas/Ejercicio2.java
@@ -6,35 +6,35 @@ public class Ejercicio2 {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		
+
 		Integer numero;
-		
+
 		do {
 			System.out.println("Introduzca un numero del 2 al 10");
 			numero = scanner.nextInt();
 		} while (numero >= 2 && numero <= 10);
-		
+
 		scanner.nextLine();
-		
+
 		String silaba;
-		
+
 		do {
 			System.out.println("Dme una palabra de tamaño 2");
 			silaba = scanner.nextLine();
 		} while (silaba.length() != 2);
-		
+
 		String palabra;
-		
+
 		do {
 			System.out.println("Dame una palabra magica");
 			palabra = scanner.nextLine();
-			if ( palabra.length() < numero) {
+			if (palabra.length() < numero) {
 				System.out.println("La palabra es menor a " + numero);
-			}else if ( palabra.length() > numero) {
+			} else if (palabra.length() > numero) {
 				System.out.println("La palabra es mayor a " + numero);
 			}
-		}while (palabra.length() == numero && palabra.endsWith(silaba));
-		
+		} while (palabra.length() == numero && palabra.endsWith(silaba));
+
 		scanner.close();
 	}
 
