26d847853788fe34d62a443cf067e79c649b5b53
“Fabio” <yesares2003@gmail.com>
2022-12-19 16:19:37 +0100

    Ejercicio1

--- Ejercicios_Recuperacion/src/programas/Ejercicio1.java
+++ Ejercicios_Recuperacion/src/programas/Ejercicio1.java
@@ -1,35 +1,34 @@
 package programas;
 
-
 import java.util.Scanner;
 
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		
+
 		System.out.println("Dame una lista de nombres separados por ;");
 		String nombre = scanner.nextLine();
-		
+
 		String nombres[] = nombre.split(";");
 		Integer edades[] = new Integer[nombres.length];
-		
+
 		for (int i = 0; i < nombres.length; i++) {
 			Integer edad = 0;
-			
+
 			do {
 				System.out.println("Dame la edad de " + nombres[i]);
 				edad = scanner.nextInt();
 			} while (!(edad >= 18 && edad < 100));
 			edades[i] = edad;
 		}
-		
-		 for (int i = 0; i < edades.length; i++) {
+
+		for (int i = 0; i < edades.length; i++) {
 			System.out.println(nombres[i] + " tiene " + edades[i]);
 		}
-		
+
 		scanner.close();
-		
+
 	}
 
 }
