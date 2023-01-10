fa7f6aaa314d8ded2acde23500cecbce57656f54
Fabio <yesares2003@gmail.com>
2023-01-10 18:36:17 +0100

    Ejercicios Tema4 prueba

---
+++ EjerciciosTema4/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author TECH
+ *
+ */
+module EjerciciosTema4 {
+}
---
+++ EjerciciosTema4/src/programas/prueba.java
@@ -0,0 +1,8 @@
+package programas;
+
+public class prueba {
+	public static void main(String[] args) {
+		System.out.println("Ejercicios Tema4");
+	}
+
+}
