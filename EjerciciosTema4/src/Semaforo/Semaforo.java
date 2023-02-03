166f5656fb72b55f1948976888ae32d7ae6c7539
Pablo <paldegunde0328@ceuandalucia.es>
2023-02-03 16:59:11 +0100

    .

--- EjerciciosTema4/src/Semaforo/Semaforo.java
+++ EjerciciosTema4/src/Semaforo/Semaforo.java
@@ -3,11 +3,70 @@ package Semaforo;
 public class Semaforo {
 	private colores color;
 	private Boolean parpadeo;
-	private enum colores{ROJO , VERDE , AMBAR}
+	public static enum colores{ROJO , VERDE , AMBAR}
 	
 	public Semaforo() {
 		color = colores.ROJO;
-	};
+		parpadeo = false;
+	}
+
+	public colores getColor() {
+		return color;
+	}
+
+	public void setColor(colores color) {
+		if (color == colores.AMBAR || color == colores.ROJO || color == colores.VERDE){
+			System.out.println("Color " + color);
+			this.color = color;
+		}else {
+			System.out.println("Color no valido");
+		}
+		}
+	
+
+	public Boolean getParpadeo() {
+		return parpadeo;
+	}
+
+	public void setParpadeo(Boolean parpadeo) {
+		if (!color.equals(colores.AMBAR)) {
+			this.parpadeo = false;
+		}else if ( color.equals(colores.AMBAR)) {
+			this.parpadeo = parpadeo;
+		}
+	}
+	
+	public void cambiarEstado() {
+		if ( color.equals(colores.VERDE)) {
+			parpadeo = true;
+			color = colores.AMBAR;
+		}else if ( color.equals(colores.AMBAR) && parpadeo) {
+			color = colores.AMBAR;
+			parpadeo = false;
+		}else if ( color.equals(colores.AMBAR) && !parpadeo) {
+			color = colores.ROJO;
+		}else if ( color.equals(colores.ROJO)) {
+			color = colores.VERDE;
+		}
+	}
+
+	@Override
+	public String toString() {
+		if ( parpadeo == true) {
+			return "Semaforo en ambar parpadeando";
+		}else {
+			return "Semaforo [color=" + color +"]";
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
