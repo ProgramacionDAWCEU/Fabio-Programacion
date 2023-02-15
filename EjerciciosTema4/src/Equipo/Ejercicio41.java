df4f08a47c1f4ccf3e061f9d49c54c1ee050af31
Pacheco <yesares2003@gmail.com>
2023-02-15 10:51:24 +0100

    .

--- EjerciciosTema4/src/Equipo/Ejercicio41.java
+++ EjerciciosTema4/src/Equipo/Ejercicio41.java
@@ -11,7 +11,7 @@ public class Ejercicio41 {
 		Equipo equipo1 = new Equipo("equipo1");
 		Equipo equipo2 = new Equipo("equipo2");
 		
-		
+		//Se crean los dos equipos
 		List<Jugador> jugadores1 = new ArrayList<>();
 		
 		for ( int i = 0; i < 3; i++) {
@@ -34,6 +34,17 @@ public class Ejercicio41 {
 			Jugador jugador = new Jugador(nombre,dorsal);
 			jugadores1.add(jugador);
 		}
+		
+		
+		//Se crea el partido con resultado de 0-0
+		
+		Partido partido = new Partido();
+		partido.setEquipoLocal(equipo1);
+		partido.setEquipoLocal(equipo2);
+		
+		Resultado resultado = new Resultado(1,0);
+		partido.setResultado(resultado);
+		System.out.println(resultado.toString());
 	}
 
 }
