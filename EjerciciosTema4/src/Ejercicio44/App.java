a640bdc34b3f4023c80d9c2f9a6c205dcd0a948b
Pablo <paldegunde0328@ceuandalucia.es>
2023-02-17 16:58:11 +0100

    .

--- EjerciciosTema4/src/Ejercicio44/App.java
+++ EjerciciosTema4/src/Ejercicio44/App.java
@@ -7,6 +7,12 @@ public class App {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
+		System.out.println("Tipos de personajes");
+		System.out.println("1. Asesinos");
+		System.out.println("2. Parasitos");
+		
+		int contadorCodigo;
+		
 		
 	}
 
