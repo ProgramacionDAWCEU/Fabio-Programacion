59e484f1b3290508566f932d4f25db482fb1c60b
“Fabio” <yesares2003@gmail.com>
2023-02-21 17:57:46 +0100

    .

--- EjerciciosTema4/src/Dates/Ejercicio45.java
+++ EjerciciosTema4/src/Dates/Ejercicio45.java
@@ -1,10 +1,35 @@
 package Dates;
 
+import java.time.LocalDate;
+import java.util.Scanner;
+
 public class Ejercicio45 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		Boolean comprobarFecha;
+		LocalDate fecha;
+		do {
+			System.out.println("Fecha de nacimiento");
+			int dia = sc.nextInt();
+			int mes = sc.nextInt();
+			int anio = sc.nextInt();
+			fecha = LocalDate.of(dia, mes, anio);
+			comprobarFecha = comprobar(fecha);
+			
+		} while (comprobarFecha == false);
+		
+		Boolean anioBisiesto = fecha.isLeapYear(); 
+		
 
+	}
+	
+	public static Boolean comprobar(LocalDate fecha) {
+		if ( fecha.isAfter(LocalDate.now()) ) {
+			return false;
+		}else {
+			return true;
+		}
 	}
 
 }
