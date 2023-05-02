4c5fc576f35cfb91999a8ed66b71ec1723047ea7
Pablo <paldegunde0328@ceuandalucia.es>
2023-05-02 20:56:38 +0200

    .

--- EjerciciosTema06/src/Ejercicio1/App.java
+++ EjerciciosTema06/src/Ejercicio1/App.java
@@ -1,24 +1,56 @@
 package Ejercicio1;
 
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Scanner;
 
 public class App {
 
 	public static void main(String[] args) throws SQLException {
-		// Se añade filtro del ejercicio 2.
 
+		Integer y = menu();
+		if (y == 1) {
+			consultarPersona();
+		} else if (y == 2) {
+			aplicarFiltro();
+		} else if (y == 3) {
+			insertarPersona();
+		} else if (y == 4) {
+			borrarPersona();
+		} else if (y == 5) {
+			actualizarPersona();
+		}
+
+	}
+
+	public static Integer menu() {
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Consultar persona");
+		System.out.println("Aplicar filtro");
+		System.out.println("Insertar persona");
+		System.out.println("Borrar persona");
+		System.out.println("Actualizar persona");
+		Integer x = sc.nextInt();
+		return x;
+	}
+
+	public static void consultarPersona() throws SQLException {
 		PersonaService service = new PersonaService();
 		Persona p = new Persona();
-		String dni = "789";
-		String filtro = "";
 
+		String dni = "789";
 		p = service.consultarPersonas(dni);
 		System.out.println("Esta es la persona con el dni indicado");
 		System.out.println(p.toString());
+	}
 
+	public static void aplicarFiltro() throws SQLException {
+		Scanner sc = new Scanner(System.in);
+		PersonaService service = new PersonaService();
+		Persona p = new Persona();
+
+		String filtro = "";
 		System.out.println("Indique filtro");
 		filtro = sc.nextLine();
 		List<Persona> lista = service.buscarPersonas(filtro);
@@ -27,6 +59,50 @@ public class App {
 		}
 
 		System.out.println(p.toString());
+	}
+
+	public static void insertarPersona() throws SQLException {
+		Scanner sc = new Scanner(System.in);
+		PersonaService service = new PersonaService();
+		Persona p = new Persona();
+
+		System.out.println("Indique nombre de la persona");
+		p.setNombre(sc.nextLine());
+		System.out.println("Indique nombre de la persona");
+		p.setApellidos(sc.nextLine());
+		System.out.println("Indique dni");
+		p.setDni(sc.nextLine());
+		p.setFecha_nacimiento(LocalDate.now());
+		service.insertarPersona(p);
+	}
+
+	public static void borrarPersona() throws SQLException {
+		Scanner sc = new Scanner(System.in);
+		PersonaService service = new PersonaService();
+		Persona p = new Persona();
+
+		System.out.println("Indique el dni de la persona a borrar");
+		p.setDni(sc.nextLine());
+		service.borrarPersona(p);
+		System.out.println("Usuario borrado con exito");
+
+	}
+
+	public static void actualizarPersona() throws SQLException {
+		Scanner sc = new Scanner(System.in);
+		PersonaService service = new PersonaService();
+		Persona p = new Persona();
+
+		System.out.println("Dni de persona a actualizar");
+		String dni = sc.nextLine();
+		System.out.println("Ingrese nuevo nombre");
+		p.setNombre(sc.nextLine());
+		System.out.println("Ingrese nuevo apellido");
+		p.setApellidos(sc.nextLine());
+		p.setFecha_nacimiento(LocalDate.now());
+		System.out.println("Ingrese nuevo dni");
+		p.setDni(sc.nextLine());
+		service.actualizarPersona(p, dni);
 	}
 
 }
--- EjerciciosTema06/src/Ejercicio1/PersonaService.java
+++ EjerciciosTema06/src/Ejercicio1/PersonaService.java
@@ -2,9 +2,11 @@ package Ejercicio1;
 
 import java.sql.Connection;
 import java.sql.Date;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -93,4 +95,81 @@ public class PersonaService {
 
 	}
 
+	public void insertarPersona(Persona persona) throws SQLException {
+
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		try {
+			Main proveedor = new Main();
+			conn = proveedor.getConnection();
+
+			String sql = "INSERT INTO PERSONAS VALUES (?,?,?,?)";
+			stmt = conn.prepareStatement(sql);
+			stmt.setString(1, persona.getDni());
+			stmt.setString(2, persona.getNombre());
+			stmt.setString(3, persona.getApellidos());
+			stmt.setDate(4, Date.valueOf(persona.getFecha_nacimiento()));
+
+			stmt.execute();
+		} catch (DateTimeException e) {
+			System.err.println(e.getMessage());
+		} finally {
+			if (stmt != null) {
+				stmt.close();
+			}
+			if (conn != null) {
+				conn.close();
+			}
+		}
+	}
+
+	public void borrarPersona(Persona persona) throws SQLException {
+		Connection conn = null;
+		PreparedStatement stmt = null;
+
+		try {
+			Main proveedor = new Main();
+			conn = proveedor.getConnection();
+
+			String sql = "DELETE FROM PERSONAS WHERE DNI = (?)";
+			stmt = conn.prepareStatement(sql);
+			stmt.setString(1, persona.getDni());
+			stmt.execute();
+
+		} finally {
+			if (stmt != null) {
+				stmt.close();
+			}
+			if (conn != null) {
+				conn.close();
+			}
+		}
+	}
+
+	public void actualizarPersona(Persona persona, String dni) throws SQLException {
+		Connection conn = null;
+		PreparedStatement stmt = null;
+
+		try {
+			Main proveedor = new Main();
+			conn = proveedor.getConnection();
+
+			String sql = "UPDATE PERSONAS SET NOMBRE = (?) , APELLIDOS = (?) , FECHA_NACIMIENTO = (?) , DNI = (?) WHERE DNI = (?)";
+			stmt = conn.prepareStatement(sql);
+			stmt.setString(1, persona.getNombre());
+			stmt.setString(2, persona.getApellidos());
+			stmt.setDate(3, Date.valueOf(persona.getFecha_nacimiento()));
+			stmt.setString(4, persona.getDni());
+			stmt.setString(5, persona.getDni());
+			stmt.execute();
+
+		} finally {
+			if (stmt != null) {
+				stmt.close();
+			}
+			if (conn != null) {
+				conn.close();
+			}
+		}
+	}
 }
